d3586746a5b6c3532878e51bdd1ead02ccfa61c7
jduo <jduo@juju.com>
2021-12-08 02:50:04 -0300

    Extract customization function

--- main.py
+++ main.py
@@ -46,48 +46,45 @@ class Server:
         cid = self.cid
         logging.info('Got connection from client cid={cid}', cid=cid)
         try:
-            try:
-                request = await asyncio.wait_for(sreader.readline(), self.timeout)
-                request_trailer = await asyncio.wait_for(sreader.read(-1), self.timeout)
-                logging.info('request={request!r}, cid={cid}', request=request, cid=cid)
-            except asyncio.TimeoutError:
-                request = b''
-            if request == b'':
-                raise OSError
+            request = await asyncio.wait_for(sreader.readline(), self.timeout)
+            request_trailer = await asyncio.wait_for(sreader.read(-1), self.timeout)
+            logging.info('request={request!r}, cid={cid}', request=request, cid=cid)
             verb, path = request.split()[0:2]
-            logging.info(path)
-            if path == b'/favicon.ico':
-                status = 404
-                msg = 'NOT FOUND'
-                content = ''
-            elif path == b'/task_list':
-                if verb == b'POST':
-                    payload = request_trailer[request_trailer.rfind(b'\r\n\r\n')+4:].decode('utf8')
-                    riego.task_list.load_tasks(ujson.loads(payload))
-                status = 200
-                msg = 'OK'
-                content = str(riego.task_list.table_json)
-            else:
-                status = 200
-                msg = 'OK'
-                content = web_page()
-            header = ('HTTP/1.1 %s %s\n' % (status, msg) +
-                      'Content-Type: text/html\n'
-                      'Connection: close\n\n')
+            header, content = serve_requests(verb, path, request_trailer)
             swriter.write(header + content)
             await swriter.drain()
-        except OSError:
-            pass
+        except Exception as e:
+            logging.info('Exception e={e}', e=e)
         logging.info('Client {cid} disconnect.', cid=cid)
         swriter.close()
         await swriter.wait_closed()
         logging.info('Client {cid} socket closed.', cid=cid)
+        riego.garbage_collect()
 
     async def close(self):
         logging.info('Closing server')
         self.server.close()
         await self.server.wait_closed()
         logging.info('Server closed.')
+
+
+def serve_requests(verb, path, request_trailer):
+    logging.info(path)
+    if path == b'/task_list':
+        if verb == b'POST':
+            payload = request_trailer[request_trailer.rfind(b'\r\n\r\n')+4:].decode('utf8')
+            riego.task_list.load_tasks(ujson.loads(payload))
+        status = 200
+        msg = 'OK'
+        content = ujson.dumps(riego.task_list.table_json)
+    else:
+        status = 404
+        msg = 'NOT FOUND'
+        content = ''
+    header = ('HTTP/1.1 %s %s\n' % (status, msg) +
+              'Content-Type: text/html\n'
+              'Connection: close\n\n')
+    return header, content
 
 
 def main():
@@ -100,5 +97,6 @@ def main():
         _ = asyncio.new_event_loop()
 
 main()
+
 
 
