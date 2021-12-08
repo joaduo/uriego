42786c4e6b5f0220cfc213f3381d7dfaec0bd1bc
jduo <jduo@juju.com>
2021-12-08 05:01:36 -0300

    Clean

--- boot.py
+++ boot.py
@@ -7,11 +7,19 @@ station = network.WLAN(network.AP_IF)
 print('Connection successful')
 print(station.ifconfig())
 
-# ap = network.WLAN(network.AP_IF)
-# 
-# #ap.active(True)
-# #ap.config(essid='uriego', password='TuT0t0t0TiburonPapazzz23495')
-# #ap.active(True)
-# 
-# print('Connection successful')
-# print(ap.ifconfig())
+ap = network.WLAN(network.AP_IF)
+#ap.active(True)
+#ap.config(essid='uriego', password='')
+#ap.active(True)
+print('Connection successful')
+print(ap.ifconfig())
+
+
+#station = network.WLAN(network.STA_IF)
+#station.active(False)
+
+#ssid = 'motojoa'
+#password = ''
+#station.active(True)
+#station.connect(ssid, password)
+#print(station.ifconfig())
