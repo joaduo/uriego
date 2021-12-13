1a1e0dccbd399d3dded4e90635a370d691d83ec1
jduo <jduo@juju.com>
2021-12-13 08:10:14 -0300

    Prepare tasks before sending

--- riegocli.py
+++ riegocli.py
@@ -1,19 +1,64 @@
 import requests
 import json
 import time
+from pprint import pprint
 
 
 URL_BASE='http://192.168.4.1/'
 AUTH_TOKEN='1234'
 
+
+def weekday_to_int(d):
+    try:
+        d = int(d)
+        assert 0 <= d <= 6
+        return d
+    except ValueError:
+        return ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'
+                ].index(d.lower()[:3])
+
+def month_to_int(d):
+    try:
+        d = int(d)
+        assert 0 < d <= 12
+        return d
+    except ValueError:
+        return ['jan', 'feb', 'mar', 'apr', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dic'
+                ].index(d.lower()[:3]) + 1
+
+def to_md_tuple(md_str):
+    from_m, from_d = md_str.split(',')
+    return month_to_int(from_m), int(from_d)
+
+def to_int_weekdays(dlist):
+    return list(sorted(weekday_to_int(d)
+                       for d in dlist))
+
+def to_hms(hms_str):
+    return tuple(int(s) for s in hms_str.split(':'))
+
+
 def build_task_list():
     pl = [
-        { 
-          "name":"test",
-          "start":"00:00:00",
-          "end":"00:00:40",
-          "week_days":["Mon", "Wed", "Fri", "Sun"],
-          "from_day":"Dic,1", "to_day":"Jan,2",
+        {
+          "name":"abajo",
+          "start": to_hms("00:00:00"),
+          "end": to_hms("00:00:5"),
+          "week_days":to_int_weekdays(
+              ["Mon", "Wed", "Fri", "Sun"]),
+          "from_day":to_md_tuple("Dic,1"),
+          "to_day":to_md_tuple("Jan,2"),
+          "gate":0,
+          "pump":0 #ignored by now
+        },
+        {
+          "name":"arriba",
+          "start": to_hms("00:00:00"),
+          "end": to_hms("00:00:3"),
+          "week_days":to_int_weekdays(
+              ["Mon", "Wed", "Fri", "Sun"]),
+          "from_day":to_md_tuple("Dic,1"),
+          "to_day":to_md_tuple("Jan,2"),
           "gate":1,
           "pump":0 #ignored by now
         },
@@ -45,8 +90,8 @@ def send_task_list(task_list):
     endpoint = 'task_list'
     r1 = requests.get(URL_BASE + endpoint)
     r2 = post(endpoint, task_list)
-#     print(r1.text, r2.text)
-    print(r1.json(), r2.json())
+    print(r1.text, r2.text)
+#     print(r1.json(), r2.json())
 
 
 def verify_time():
@@ -64,11 +109,12 @@ def verify_time():
         print(f'Device time is correct {remote_t}')
 
 
-def trigger_task(name):
+def trigger_tasks(*names):
     endpoint = 'manual_task'
     r1 = get(endpoint)
-    r2 = post(endpoint, [name])
-    print(r1.json(), r2.json())
+    r2 = post(endpoint, names)
+#     print(r1.json(), r2.json())
+    print(r1.text, r2.text)
     return r2
 
 
@@ -80,11 +126,12 @@ def test_auth_token():
 
 
 def main():
+#     pprint(build_task_list())
 #     test_task_list()
 #     test_auth_token()
-    verify_time()
+#     verify_time()
     send_task_list(build_task_list())
-    trigger_task('test')
+    trigger_tasks('arriba', 'abajo')
 
 
 if __name__ == '__main__':
