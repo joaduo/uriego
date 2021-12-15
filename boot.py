bf349b23fbc42e6914fc86594c0e0a1e06151ecb
jduo <jduo@juju.com>
2021-12-15 02:34:08 -0300

    Simplify boot

--- boot.py
+++ boot.py
@@ -1,25 +1,7 @@
 import network
 import esp ; esp.osdebug(None)
-import gc; gc.collect()
-
-station = network.WLAN(network.AP_IF)
-
-print('Connection successful')
-print(station.ifconfig())
+import gc; gc.collect() ; print(gc.mem_free())
 
 ap = network.WLAN(network.AP_IF)
-#ap.active(True)
-#ap.config(essid='uriego', password='')
-#ap.active(True)
-print('Connection successful')
+print('AP config:')
 print(ap.ifconfig())
-
-
-#station = network.WLAN(network.STA_IF)
-#station.active(False)
-
-#ssid = 'motojoa'
-#password = ''
-#station.active(True)
-#station.connect(ssid, password)
-#print(station.ifconfig())
