cff23e3fecf654ae386dd7883b556a578992e68f
jduo <joaduo@gmail.com>
2022-09-04 01:25:34 -0300

    Add separate valve relay

--- devices.py
+++ devices.py
@@ -11,7 +11,8 @@ import log
 #D7  GPIO13  OK  OK  SPI (MOSI)
 #D8  GPIO15  pulled to GND OK  SPI (CS)
 PUMP_PIN_NUMBER=14
-VALVE_PIN_NUMBER=12
+VALVE_1_PIN_NUMBER=12
+VALVE_2_PIN_NUMBER=13
 
 
 class InvertedPin(machine.Pin):
@@ -31,23 +32,27 @@ class InvertedPin(machine.Pin):
 
 
 class Pump:
-    def __init__(self, pump_pin_n=PUMP_PIN_NUMBER, valve_pin_n=VALVE_PIN_NUMBER):
+    def __init__(self, pump_pin_n=PUMP_PIN_NUMBER,
+                 valve_1_pin=VALVE_1_PIN_NUMBER,
+                 valve_2_pin=VALVE_2_PIN_NUMBER):
         self.pump_out = InvertedPin(pump_pin_n, machine.Pin.OUT, value=1)
-        self.valve_out = InvertedPin(valve_pin_n, machine.Pin.OUT, value=1)
+        self.valve_1 = InvertedPin(valve_1_pin, machine.Pin.OUT, value=1)
+        self.valve_2 = InvertedPin(valve_2_pin, machine.Pin.OUT, value=1)
+        self.valve_map = {0:self.valve_1, 1:self.valve_2}
         self.stop()
     def start(self, valve_value):
         if self.pump_out.value():
             self.stop()
             log.error('Pump already running')
             return False
-        self.valve_out.value(valve_value)
-        utime.sleep(0.5) # give time to the relay to flip
+        valve = self.valve_map[valve_value]
+        valve.on()
         self.pump_out.on()
         return self.pump_out.value()
     def stop(self):
+        for v in self.valve_map.values():
+            v.off()
         self.pump_out.off()
-        utime.sleep(0.5) # give time to the relay to flip
-        self.valve_out.off()
         return not self.pump_out.value()
     def monitor(self, running):
         if running:
